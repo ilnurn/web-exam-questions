3892820078f4f2bf1c4bcd2f9a2c0c606aeaa5b2
Ilnur <nabiulli@mail.ru>
2022-05-08 21:32:38 +0500

    Initial commit

---
+++ src/test/java/pro/sky/java/course2/webexamquestions/WebExamQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.webexamquestions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebExamQuestionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
